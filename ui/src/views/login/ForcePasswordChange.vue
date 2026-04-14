f68eda337e9140128b71a6b983969cd620a17c5d
Ubuntu <ubuntu@localhost.localdomain>
2026-04-14 10:44:23 +0800

    feat(ui): add forced password change route gating
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ ui/src/views/login/ForcePasswordChange.vue
@@ -0,0 +1,17 @@
+<template>
+  <login-layout>
+    <LoginContainer>
+      <h2 class="mb-24">{{ $t('views.login.resetPassword') }}</h2>
+      <p class="mb-24 lighter">{{ $t('views.login.newPassword') }}</p>
+      <CurrentPasswordResetForm forced />
+    </LoginContainer>
+  </login-layout>
+</template>
+
+<script setup lang="ts">
+import LoginContainer from '@/layout/login-layout/LoginContainer.vue'
+import LoginLayout from '@/layout/login-layout/LoginLayout.vue'
+import CurrentPasswordResetForm from '@/components/user/CurrentPasswordResetForm.vue'
+</script>
+
+<style lang="scss" scoped></style>
